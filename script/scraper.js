9f18f3f6211cdf6d19ed12878609553eea9b5319
ChristopherYammine <chrisyammine98@gmail.com>
2022-09-21 03:42:56 +0300

    displaying the users array

--- script/scraper.js
+++ script/scraper.js
@@ -1,19 +1,38 @@
 
-axios({
-    method: "GET",
-    url: `https://api.allorigins.win/get?url=${encodeURIComponent('https://www.hackerrank.com/hicham_hachem1?hr_r=1')}`,
+let user_info = [];
+
+function getUserPages(hackerRank_username) {
+    axios({
+        method: "GET",
+        url: `https://api.allorigins.win/get?url=${encodeURIComponent('https://www.hackerrank.com/')}` + hackerRank_username,
 
 
-}).then(function (res) {
-    let htmlbody = document.getElementById("output");
-    let url_content = res.data.contents;
+    }).then(function (res) {
+        let init_htmlbody = document.getElementById("initial-output");
+        let url_content = res.data.contents;
 
 
-    htmlbody.innerHTML += url_content;
-    let badge = document.getElementsByClassName("section-card hacker-badges")[0].innerHTML;
-    let username = document.getElementsByClassName("profile-username-heading")[0].innerHTML;
-    let name = document.getElementsByClassName("profile-heading")[0].innerHTML;
-    htmlbody.innerHTML = "";
-    htmlbody.innerHTML += `<div class="user-container">${username}  ${name}  ${badge}</div>`;
+        init_htmlbody.innerHTML += url_content;
+        let badge = document.getElementsByClassName("section-card-content")[0].innerHTML;
+        let username = document.getElementsByClassName("profile-username-heading")[0].innerHTML;
+        let name = document.getElementsByClassName("profile-heading")[0].innerHTML;
 
-})
+
+        init_htmlbody.innerHTML = "";
+        let scraped_info = `<div class="user-container">${username}  ${name}  ${badge}</div>`;
+        user_info.push(scraped_info);
+    })
+}
+
+
+let final_output = document.getElementById('final-output');
+setTimeout(() => {
+    for (let i = 0; i < user_info.length; i++) {
+        let element = user_info[i];
+        final_output.innerHTML += element;
+
+    }
+}, 1000);
+
+
+
